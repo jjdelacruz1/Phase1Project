8dd351c77624f65884fd57d0a43f83f7f1dc7ab5
Sue Park <suepandrice06@gmail.com>
2019-08-24 23:28:03 -0500

    test for Yelp search endpoint

--- homepage.js
+++ homepage.js
@@ -1,1 +1,24 @@
 // javascript info
+// $(document).ready(function() {
+    let apiKey = 'Bearer fpfUJj8DFp_jm-n0LNi5U4WL9AgyD3G2ieoAPAYccY2QUi-1ZCXSuHoa0uEaPY60BInSS_COQHHlqWp0VeKDOcgdPBHn9lYSC1_r6mJCI3y8aU63IHNfK6Lhr3xhXXYx';
+    let corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com'
+    let yelpSearchurl = 'https://api.yelp.com/v3/businesses/search';
+    let requestObj = {
+        url: corsAnywhereUrl + '/' + yelpSearchurl,
+        data: {term: 'happy hour', location: '77024'},
+        headers: {'Authorization': apiKey},
+        error: function(jqXHR, textStatus, errorThrown) {
+            console.log('AJAX error, jqXHR =', jqXHR, ', textStatus = ',
+                textStatus, ', errorThrown = ', errorThrown)
+        }
+    }
+
+    console.log('bambs')
+    $.ajax(requestObj)
+        .done(function(response) {
+            console.log('typeof response = ' + typeof response)
+            console.log('response = ', response)
+        })
+
+
+    // });
