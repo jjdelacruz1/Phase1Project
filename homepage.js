53331cc7e190f3eea11e0d42814de9032a6d8549
Sue Park <suepandrice06@gmail.com>
2019-08-27 00:58:00 -0500

    connected markers to yelp search results

--- homepage.js
+++ homepage.js
@@ -1,6 +1,14 @@
 let ajaxResponse = null
 
 window.addEventListener('DOMContentLoaded', function () {
+
+    L.mapbox.accessToken = 'pk.eyJ1Ijoic3VlcGFyazA5IiwiYSI6ImNqenJmdGxoNzBqengzbW8zeDlmNnhudHEifQ.NvYx9iu9NUGdvDdYdWNg-A';
+
+    let map = L.mapbox.map('map')
+    .setView([29.76328, -95.36327], 12)
+    .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
+    
+
     let searchTerm = 'sushi'
     let zipCode = '77007'
     let yelpApiKey = 'Bearer fpfUJj8DFp_jm-n0LNi5U4WL9AgyD3G2ieoAPAYccY2QUi-1ZCXSuHoa0uEaPY60BInSS_COQHHlqWp0VeKDOcgdPBHn9lYSC1_r6mJCI3y8aU63IHNfK6Lhr3xhXXYx'
@@ -14,14 +22,30 @@ window.addEventListener('DOMContentLoaded', function () {
     $.ajax(yelpAjaxRequest)
         .then(function (response) {
             console.log('This is returned from the AJAX request: ',response)
+            //Code for the map markers and popup
+            for (let i = 0; i < response.businesses.length; i++) {
+                let coordinate = response.businesses[i].coordinates;
+                let singleBusiness = response.businesses[i];
+                L.marker([coordinate.latitude, coordinate.longitude], 
+                    {title: singleBusiness.name}).addTo(map)
+                    .bindPopup(`
+                            <div class="img"><img src="${singleBusiness.image_url}" height="50px"></div>
+                            <h4>${singleBusiness.name}</h4>
+                            ${singleBusiness.location.display_address.join("<br>")}         
+                        `);
+            }
+           
+
             ajaxResponse = response.businesses
             let bodyContainer = document.querySelector('.container')
             bodyContainer.innerHTML = createYelpResultsHtml(ajaxResponse)
             //console.log('This is saved into the empty array: ', ajaxResponse)
     });
 
+   
     function createYelpResultsHtml (yelpSearchResults) {
         let businessHtml = yelpSearchResults.map(function (singleBusiness) {
+
             return `<div class='card'></div><img class='card-image-top' style="width: 200px;" src='${singleBusiness.image_url}'>
             <div class='body'>
             <h5 class='card-title'>${singleBusiness.name}</h5>
@@ -30,4 +54,7 @@ window.addEventListener('DOMContentLoaded', function () {
         })
         return businessHtml.join('')
     }
+
+
+   
 })
