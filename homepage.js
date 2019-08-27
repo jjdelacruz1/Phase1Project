8af214b317bd9ac8303a45da659ad729a0c2ec5c
Alvin Ng <ngalvin93@gmail.com>
2019-08-26 22:59:24 -0500

    added some render details

--- homepage.js
+++ homepage.js
@@ -22,20 +22,49 @@ window.addEventListener('DOMContentLoaded', function () {
 
     function createYelpResultsHtml (yelpSearchResults) {
         let businessHtml = yelpSearchResults.map(function (singleBusiness) {
+            // let openStatus = ''
+            // if (singleBusiness.is_closed === false) {
+            //     openStatus = '<a href="#" class="badge badge-success">Open</a>'
+            // } else {
+            //     openStatus = '<a href="#" class="badge badge-secondary">Closed</a>'
+            // }
+            let starRating = ''
+            if (singleBusiness.rating === 5) {
+                starRating = '<img src="/img/regular_5.png">'
+            } else if (singleBusiness.rating === 4.5) {
+                starRating = '<img src="/img/regular_4_half.png">'
+            } else if (singleBusiness.rating === 4) {
+                starRating = '<img src="/img/regular_4.png">'
+            } else if (singleBusiness.rating === 3.5) {
+                starRating = '<img src="/img/regular_3_half.png">'
+            } else if (singleBusiness.rating === 3) {
+                starRating = '<img src="/img/regular_3.png">'
+            } else if (singleBusiness.rating === 2.5) {
+                starRating = '<img src="/img/regular_2_half.png">'
+            } else if (singleBusiness.rating === 2) {
+                starRating = '<img src="/img/regular_2.png">'
+            } else if (singleBusiness.rating === 1.5) {
+                starRating = '<img src="/img/regular_1_half.png">'
+            } else if (singleBusiness.rating === 1) {
+                starRating = '<img src="/img/regular_1.png">'
+            } else {
+                starRating = '<img src="/img/regular_0.png">'
+            }
             return `          
             <div class='card mb-3'>
                 <div class="row no-gutters">
-                    <div class="col-md-4">
-                        <img class='card-img' src='${singleBusiness.image_url}'>
+                    <div class="col-md-3">
+                        <a href="${singleBusiness.url}"><img class='business-img card-img rounded' src='${singleBusiness.image_url}'></a>
                     </div>
                     <div class="col-md-8">
-                    <div class="card-body">
-                        <h3 class='card-title'>${singleBusiness.name}</h3>
-                        <p>${singleBusiness.location.display_address[0]}<br>${singleBusiness.location.display_address[2]}</p>
+                        <div class="card-body">
+                            <a href="${singleBusiness.url}"><h5 class='card-title'>${singleBusiness.name}</h5></a>
+                            <p id='price-review'>${singleBusiness.price} • ${starRating} • ${singleBusiness.review_count} reviews</p>
+                            <p>${singleBusiness.location.display_address[0]}<br>${singleBusiness.location.display_address[2]}</p>
+                        </div>
                     </div>
-                    </div>
-                    </div>
-                    </div>
+                </div>
+            </div>
             `
         })
         return businessHtml.join('')
