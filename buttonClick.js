1a584f8bd2ead51deef15b3ef4e5c304ca2ab563
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-08-28 12:28:47 -0500

    Fixed the error where you could click anywhere and the modal would close. Also check out what happens when you click not 21!

--- buttonClick.js
+++ buttonClick.js
@@ -2,6 +2,12 @@ var wrapper = document.getElementById('mainWrapper');
 var button = document.getElementById('ok');
 
 button.addEventListener('click', function(){
-  console.log('clicked')
+  wrapper.classList.toggle("blur")
+})
+
+var button2 = document.getElementById('notok')
+
+button2.addEventListener('click', function(){
+  wrapper.innerHTML = '<h1>You are not old enough!</h1>'
   wrapper.classList.toggle("blur")
 })
