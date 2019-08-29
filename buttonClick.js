d9fe63f4d6b3e36f311426d6077df3cd45d0773c
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-08-29 01:05:08 -0500

    Added the picture and layout there are still some styling and display things that need to be fixed plus the redirect page on clicking not 21.

--- buttonClick.js
+++ buttonClick.js
@@ -8,6 +8,6 @@ button.addEventListener('click', function(){
 var button2 = document.getElementById('notok')
 
 button2.addEventListener('click', function(){
-  wrapper.innerHTML = '<h1>You are not old enough!</h1>'
+  wrapper.innerHTML = '<h1>You are not old enough!</h1><p>Testing out another line</p>'
   wrapper.classList.toggle("blur")
 })
