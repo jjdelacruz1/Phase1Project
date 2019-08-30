9e208eef2afb57251d52e05fb65231c7efa215fe
Sue Park <suepandrice06@gmail.com>
2019-08-29 20:53:50 -0500

    added node modules

--- homepage.js
+++ homepage.js
@@ -2,86 +2,14 @@ let ajaxResponse = null
 const searchTerm = 'happy hour'
 // let location = ''
 
-<<<<<<< HEAD
-
-let map = L.mapbox.map('map');
-
-document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('search-form').addEventListener('submit', function(e){
-        e.preventDefault();
-    
-        var location = document.getElementById('search-bar').value
-        var urlEncodedSearchString = encodeURIComponent(location)
-        let yelpApiKey = 'Bearer fpfUJj8DFp_jm-n0LNi5U4WL9AgyD3G2ieoAPAYccY2QUi-1ZCXSuHoa0uEaPY60BInSS_COQHHlqWp0VeKDOcgdPBHn9lYSC1_r6mJCI3y8aU63IHNfK6Lhr3xhXXYx'
-        let corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/'
-        
-        var yelpAjaxRequest = {
-        url: `${corsAnywhereUrl}https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${location}`,
-        headers: {Authorization: `${yelpApiKey}`}
-        }
-
-        $.ajax(yelpAjaxRequest)
-        .then(function (response) {
-            let bodyContainer = document.querySelector('.barContainer')
-            bodyContainer.innerHTML = ''
-            //console.log('This is returned from the AJAX request: ',response)
-            L.mapbox.accessToken = 'pk.eyJ1Ijoic3VlcGFyazA5IiwiYSI6ImNqenJmdGxoNzBqengzbW8zeDlmNnhudHEifQ.NvYx9iu9NUGdvDdYdWNg-A';
-            let viewCoordinate = response.businesses[0].coordinates;
-            map
-            .setView([viewCoordinate.latitude, viewCoordinate.longitude], 11)
-            .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'));
-
-              // if there are markers already on the map, remove the markers,
-                        // else add the markers from the results
-                    //find a way to check if the map is empty or not
-            // if (map) {
-                
-            // } 
-
-            let currentMarkers = [];
-            
-
-            if (currentMarkers!==null) {
-                for (let i = 0; i < currentMarkers.length; i++) {            
-                }
-            }
-
-            
-            console.log('Whats thiss', currentMarkers)
-            console.log('length of currentMarkers', currentMarkers.length)
-
-            currentMarkers.forEach(function(item) {
-                console.log(item)
-            })
-
-            for (let i = 0; i < response.businesses.length; i++) {
-                let coordinate = response.businesses[i].coordinates;
-                let singleBusiness = response.businesses[i];
-
-                let singleMarker = L.marker([coordinate.latitude, coordinate.longitude], 
-                    {title: singleBusiness.name}).addTo(map).bindPopup(`
-                            <div class="img"><img src="${singleBusiness.image_url}" height="50px"></div>
-                            <h4>${singleBusiness.name}</h4>
-                            ${singleBusiness.location.display_address.join("<br>")}         
-                        `);
-
-                currentMarkers.push(singleMarker)
-
-                }
-       
-            ajaxResponse = response.businesses
-            bodyContainer.innerHTML = createYelpResultsHtml(ajaxResponse)
-            //console.log('This is saved into the empty array: ', ajaxResponse)
-    });
-    })
-=======
 const map = L.mapbox.map('map')
 
 document.addEventListener('DOMContentLoaded', function () {
   document.getElementById('search-form').addEventListener('submit', function (e) {
     e.preventDefault()
+
     var location = document.getElementById('search-bar').value
-    // var urlEncodedSearchString = encodeURIComponent(location)
+    var urlEncodedSearchString = encodeURIComponent(location)
     const yelpApiKey = 'Bearer fpfUJj8DFp_jm-n0LNi5U4WL9AgyD3G2ieoAPAYccY2QUi-1ZCXSuHoa0uEaPY60BInSS_COQHHlqWp0VeKDOcgdPBHn9lYSC1_r6mJCI3y8aU63IHNfK6Lhr3xhXXYx'
     const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/'
 
@@ -100,16 +28,40 @@ document.addEventListener('DOMContentLoaded', function () {
         map
           .setView([viewCoordinate.latitude, viewCoordinate.longitude], 11)
           .addLayer(L.mapbox.styleLayer('mapbox://styles/mapbox/streets-v11'))
+
+        // if there are markers already on the map, remove the markers,
+        // else add the markers from the results
+        // find a way to check if the map is empty or not
+        // if (map) {
+
+        // }
+
+        const currentMarkers = []
+
+        if (currentMarkers !== null) {
+          for (let i = 0; i < currentMarkers.length; i++) {
+          }
+        }
+
+        console.log('Whats thiss', currentMarkers)
+        console.log('length of currentMarkers', currentMarkers.length)
+
+        currentMarkers.forEach(function (item) {
+          console.log(item)
+        })
+
         for (let i = 0; i < response.businesses.length; i++) {
           const coordinate = response.businesses[i].coordinates
           const singleBusiness = response.businesses[i]
-          L.marker([coordinate.latitude, coordinate.longitude],
-            { title: singleBusiness.name }).addTo(map)
-            .bindPopup(`
+
+          const singleMarker = L.marker([coordinate.latitude, coordinate.longitude],
+            { title: singleBusiness.name }).addTo(map).bindPopup(`
                             <div class="img"><img src="${singleBusiness.image_url}" height="50px"></div>
                             <h4>${singleBusiness.name}</h4>
                             ${singleBusiness.location.display_address.join('<br>')}         
                         `)
+
+          currentMarkers.push(singleMarker)
         }
 
         ajaxResponse = response.businesses
@@ -117,61 +69,11 @@ document.addEventListener('DOMContentLoaded', function () {
         // console.log('This is saved into the empty array: ', ajaxResponse)
       })
   })
->>>>>>> master
 })
 
-
-
 function createYelpResultsHtml (yelpSearchResults) {
-<<<<<<< HEAD
-    console.log('begin yelp func')
-    // console.log(yelpSearchResults);
-    let businessHtml = yelpSearchResults.map(function (singleBusiness) {
-        // Renders out the star rating based on number rating from json data
-        function renderStarRating () {
-            let starRating = ''
-            if (singleBusiness.rating === 5) {
-                starRating = '<img src="img/regular_5.png">'
-            } else if (singleBusiness.rating === 4.5) {
-                starRating = '<img src="img/regular_4_half.png">'
-            } else if (singleBusiness.rating === 4) {
-                starRating = '<img src="img/regular_4.png">'
-            } else if (singleBusiness.rating === 3.5) {
-                starRating = '<img src="img/regular_3_half.png">'
-            } else if (singleBusiness.rating === 3) {
-                starRating = '<img src="img/regular_3.png">'
-            } else if (singleBusiness.rating === 2.5) {
-                starRating = '<img src="img/regular_2_half.png">'
-            } else if (singleBusiness.rating === 2) {
-                starRating = '<img src="img/regular_2.png">'
-            } else if (singleBusiness.rating === 1.5) {
-                starRating = '<img src="img/regular_1_half.png">'
-            } else if (singleBusiness.rating === 1) {
-                starRating = '<img src="img/regular_1.png">'
-            } else {
-                starRating = '<img src="img/regular_0.png">'
-            }
-            return starRating
-        }
-        // Renders out the dollar signs for price range
-        function renderPriceRange () {
-            let priceRange = ''
-            if (singleBusiness.price === '$$$$') {
-                priceRange = '<span class="badge badge-pill badge-success">$$$$</span>'
-            } else if (singleBusiness.price === '$$$') {
-                priceRange = '<span class="badge badge-pill badge-success">$$$</span>'
-            } else if (singleBusiness.price === '$$') {
-                priceRange = '<span class="badge badge-pill badge-success">$$</span>'
-            } else {
-                priceRange = '<span class="badge badge-pill badge-success">$</span>'
-            }
-            return priceRange
-        }
-        // Returns the html for each business
-        return `          
-=======
   console.log('begin yelp func')
-  console.log(yelpSearchResults)
+  // console.log(yelpSearchResults);
   const businessHtml = yelpSearchResults.map(function (singleBusiness) {
     // Renders out the star rating based on number rating from json data
     function renderStarRating () {
@@ -215,7 +117,6 @@ function createYelpResultsHtml (yelpSearchResults) {
     }
     // Returns the html for each business
     return `          
->>>>>>> master
         <div id='card' class='card mb-3'>
             <div class="row no-gutters">
                 <div class="col-md-3">
