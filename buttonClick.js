2c8f75e37cde9f4850d32669e703f7ff6caf7648
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-09-03 19:13:50 -0500

    Fix the joke render on the button click from the home page to display correctly.

--- buttonClick.js
+++ buttonClick.js
@@ -9,24 +9,39 @@ var button2 = document.getElementById('notok')
 
 
 
-var randomJoke = $.ajax({
-  jsonp: false,
-  url: "http://api.icndb.com/jokes/random",
-  success: function(data) {
-    var joke = data.value.joke;
-    // console.log(data)
-    console.log(joke)
-    return joke
-  }
-})
-.then(button2.addEventListener('click', function (event) {
-  wrapper.innerHTML = `<div class="d-flex justify-content-center                           align-items-center flex-column">
-                        <h1>You are not old enough!</h1>
-                        <h2>Please get older and enjoy this joke!</h2>
-                        <h3>Random Chuck Norris Joke:</h3>
-                        <p>${event}</p>
-                        </div>`
+// var randomJoke = $.ajax({
+//   jsonp: false,
+//   url: "http://api.icndb.com/jokes/random",
+//   success: function(data) {
+//     var joke = data.value.joke;
+//     // console.log(data)
+//     console.log(joke)
+//     return joke
+//   }
+// })
+button2.addEventListener('click', function () {
+  var randomJoke = $.ajax({
+    jsonp: false,
+    url: "http://api.icndb.com/jokes/random",
+    success: function(data) {
+      var joke = data.value.joke;
+      // console.log(data)
+      console.log(joke)
+      wrapper.innerHTML = `<div class="d-flex justify-content-center                           align-items-center flex-column">
+      <h1>You are not old enough!</h1>
+      <h2>Please get older and enjoy this joke!</h2>
+      <h3>Random Chuck Norris Joke:</h3>
+      <p>${joke}</p>
+      </div>`
+    }
+  })
+  // wrapper.innerHTML = `<div class="d-flex justify-content-center                           align-items-center flex-column">
+  //                       <h1>You are not old enough!</h1>
+  //                       <h2>Please get older and enjoy this joke!</h2>
+  //                       <h3>Random Chuck Norris Joke:</h3>
+  //                       <p>${joke}</p>
+  //                       </div>`
   wrapper.classList.toggle('blur')
   console.log(event)
-}))
+})
 
