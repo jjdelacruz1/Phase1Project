daa7b8a5f1e41ea69246e10d2199573071b8acf0
jjdelacruz1 <jjdelacruz1@gmail.com>
2019-09-04 00:36:21 -0500

    Apply prettier with vscode to about pages and buttonClick.js file. Just a test to see how it can make the code look more uniform.

--- buttonClick.js
+++ buttonClick.js
@@ -1,67 +1,54 @@
-var wrapper = document.getElementById('mainWrapper')
-var button = document.getElementById('ok')
+var wrapper = document.getElementById("mainWrapper");
+var button = document.getElementById("ok");
 
-button.addEventListener('click', function () {
-  wrapper.classList.toggle('blur')
-})
+button.addEventListener("click", function() {
+  wrapper.classList.toggle("blur");
+});
 
-var button2 = document.getElementById('notok')
+var button2 = document.getElementById("notok");
 
-
-
-// var randomJoke = $.ajax({
-//   jsonp: false,
-//   url: "http://api.icndb.com/jokes/random",
-//   success: function(data) {
-//     var joke = data.value.joke;
-//     // console.log(data)
-//     console.log(joke)
-//     return joke
-//   }
-// })
-button2.addEventListener('stuff', function () {
+button2.addEventListener("stuff", function() {
   var randomJoke = $.ajax({
     jsonp: false,
     url: "http://api.icndb.com/jokes/random",
     success: function(data) {
       var joke = data.value.joke;
       // console.log(data)
-      console.log(joke)
+      console.log(joke);
       wrapper.innerHTML = `<div class="d-flex justify-content-center                           align-items-center flex-column">
       <h1>You are not old enough!</h1>
       <h2>Please get older and enjoy this joke!</h2>
       <h3>Random Chuck Norris Joke:</h3>
       <p>${joke}</p>
-      </div>`
+      </div>`;
     }
-  })
-  wrapper.classList.toggle('blur')
-})
+  });
+  wrapper.classList.toggle("blur");
+});
 
 function getJokeFromResponse(data) {
   var joke = data.value.joke;
-  return joke
+  return joke;
 }
 
 function renderJoke(joke) {
-  console.log(joke, 'this is inside the second then, chained promise')
-  wrapper.innerHTML = `<div class="d-flex justify-content-center                           align-items-center flex-column">
+  console.log(joke, "this is inside the second then, chained promise");
+  wrapper.innerHTML = `<div class="d-flex justify-content-center align-items-center flex-column">
       <h1>You are not old enough!</h1>
       <h2>Please get older and enjoy this joke!</h2>
       <h3>Random Chuck Norris Joke:</h3>
       <p>${joke}</p>
-      </div>`
-  wrapper.classList.toggle('blur')
+      </div>`;
+  wrapper.classList.toggle("blur");
 }
 
 function renderUnderPage() {
   var randomJoke = $.ajax({
     jsonp: false,
-    url: 'http://api.icndb.com/jokes/random',
+    url: "http://api.icndb.com/jokes/random"
   })
-  .then(getJokeFromResponse)
-  .then(renderJoke)
+    .then(getJokeFromResponse)
+    .then(renderJoke);
 }
 
-button2.addEventListener('click', renderUnderPage)
-
+button2.addEventListener("click", renderUnderPage);
