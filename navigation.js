d719053f05f12f2fdc585249d681779865798f16
Alvin Ng <ngalvin93@gmail.com>
2019-09-05 20:23:31 -0500

    added variables to api.js

--- navigation.js
+++ navigation.js
@@ -1,7 +1,5 @@
 function yelpAjax () {
   var location = document.getElementById("search-bar").value;
-  const yelpApiKey = "Bearer fpfUJj8DFp_jm-n0LNi5U4WL9AgyD3G2ieoAPAYccY2QUi-1ZCXSuHoa0uEaPY60BInSS_COQHHlqWp0VeKDOcgdPBHn9lYSC1_r6mJCI3y8aU63IHNfK6Lhr3xhXXYx";
-  const corsAnywhereUrl = "https://cors-anywhere.herokuapp.com/";
   var yelpAjaxInfo = {
     url: `${corsAnywhereUrl}https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${location}&limit=10`,
     headers: { Authorization: `${yelpApiKey}` }
--- results.js
+++ results.js
@@ -6,10 +6,6 @@ let markersArray = [];
 document.addEventListener("DOMContentLoaded", function() {
   const output = document.getElementById("output");
   const location = sessionStorage.getItem("location");
-
-  const yelpApiKey =
-    "Bearer fpfUJj8DFp_jm-n0LNi5U4WL9AgyD3G2ieoAPAYccY2QUi-1ZCXSuHoa0uEaPY60BInSS_COQHHlqWp0VeKDOcgdPBHn9lYSC1_r6mJCI3y8aU63IHNfK6Lhr3xhXXYx";
-  const corsAnywhereUrl = "https://cors-anywhere.herokuapp.com/";
   var yelpAjaxRequest = {
     url: `${corsAnywhereUrl}https://api.yelp.com/v3/businesses/search?term=${searchTerm}&location=${location}&limit=10`,
     headers: { Authorization: `${yelpApiKey}` }
