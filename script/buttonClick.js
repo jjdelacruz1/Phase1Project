3589ea7e72a316c8cf8a57ff4a251283bca16431
Alvin Ng <ngalvin93@gmail.com>
2019-09-06 19:41:39 -0500

    organized into script and style folder

---
+++ script/buttonClick.js
@@ -0,0 +1,37 @@
+var wrapper = document.getElementById('mainWrapper')
+var button = document.getElementById('ok')
+
+button.addEventListener('click', function () {
+  wrapper.classList.toggle('blur')
+})
+
+var button2 = document.getElementById('notok')
+
+function getJokeFromResponse(data) {
+  var joke = data.value.joke;
+  return joke;
+}
+
+function renderJoke (joke) {
+  console.log(joke, 'this is inside the second then, chained promise')
+  wrapper.innerHTML = `<div class="d-flex justify-content-center align-items-center flex-column">
+      <h1>You are not old enough!</h1>
+      <h2>Please get older and enjoy this joke!</h2>
+      <h3>Random Chuck Norris Joke:</h3>
+      <h3>${joke}</h3>
+      <br>
+      <img src=url('img/chucknorris_logo_coloured_small@2x.png')>
+      </div>`;
+  wrapper.classList.toggle("blur");
+}
+
+function renderUnderPage () {
+  var randomJoke = $.ajax({
+    jsonp: false,
+    url: 'http://api.icndb.com/jokes/random'
+  })
+    .then(getJokeFromResponse)
+    .then(renderJoke)
+}
+
+button2.addEventListener('click', renderUnderPage)
